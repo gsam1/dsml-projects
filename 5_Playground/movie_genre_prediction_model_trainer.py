b2df6090c85882e0ab62568edd54c9f7f51b882d
gsam1 <gsamardjiev@gmail.com>
2018-11-30 18:43:54 +0200

    Movie_Genre_Poster_Prediction: Added a trainer

---
+++ 5_Playground/movie_genre_prediction_model_trainer.py
@@ -0,0 +1,38 @@
+import os
+import sys
+# dl libraries
+from keras.models import Sequential
+from keras.models import Conv2D
+from keras.models import MaxPooling2D
+from keras.models import Flatten
+from keras.models import Dense
+from keras.models import load_model
+from keras.processing.image import ImageDataGenerator
+
+MODEL_FILEPATH = sys.argv[2]
+TRAIN_LOCATION = '/media/fury/data/Scripts/the_movies_data_scraper/datasets/The_Movies/posters/train'
+TEST_LOCATION = '/media/fury/data/Scripts/the_movies_data_scraper/datasets/The_Movies/posters/test'
+VALIDATION_LOCATION = '/media/fury/data/Scripts/the_movies_data_scraper/datasets/The_Movies/posters/validation'
+
+def create_generator(location):
+    '''
+    Takes the location of the images and returns a data generator
+    '''
+    datagen = ImageDataGenerator(rescale = 1./255)
+    generator = datagen.flow_from_directory(
+        location,
+        target_size = (300, 300),
+        color_mode = 'rgb',
+        batch_size = 8,
+        class_mode = 'categorical',
+        seed = 42
+    )
+
+    return generator
+)
+
+def load_model():
+    model = load_model(MODEL_FILEPATH)
+    print('Model Loaded.')
+    return model
+
