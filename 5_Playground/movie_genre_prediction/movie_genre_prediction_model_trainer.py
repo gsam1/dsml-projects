b50c817f1581ecd1d3b193f7c60d6fa05b5242ba
gsam1 <gsamardjiev@gmail.com>
2019-11-26 22:13:45 +0200

    E2E Project - faster rcnn started implementation

--- 5_Playground/movie_genre_prediction/movie_genre_prediction_model_trainer.py
+++ 5_Playground/movie_genre_prediction/movie_genre_prediction_model_trainer.py
@@ -13,9 +13,9 @@ from keras.preprocessing.image import ImageDataGenerator
 
 
 MODEL_FILEPATH = sys.argv[1]
-TRAIN_LOCATION = '/media/fury/data/Scripts/the_movies_data_scraper/datasets/The_Movies/posters/train'
-TEST_LOCATION = '/media/fury/data/Scripts/the_movies_data_scraper/datasets/The_Movies/posters/test'
-VALIDATION_LOCATION = '/media/fury/data/Scripts/the_movies_data_scraper/datasets/The_Movies/posters/validation'
+TRAIN_LOCATION = '/home/fury/Code/projects/dsml-projects/5_Playground/datasets/The_Movies/posters/train'
+TEST_LOCATION = '/home/fury/Code/projects/dsml-projects/5_Playground/datasets/The_Movies/posters/test'
+VALIDATION_LOCATION = '/home/fury/Code/projects/dsml-projects/5_Playground/datasets/The_Movies/posters/validation'
 # Check if number of epochs are defined
 try:
     EPOCHS = int(sys.argv[2])
@@ -35,27 +35,49 @@ def create_generator(location):
         class_mode = 'categorical',
     )
 
+    return generator
+
+def create_aug_generator(location):
+    '''
+    Gets the location of the images and auguments the image.
+    '''
+    datagen = ImageDataGenerator(
+        rescale = 1./255,
+        shear_range = 0.2,
+        zoom_range = 0.2,
+        horizontal_flip = True)
+
+    generator = datagen.flow_from_directory(
+        location,
+        target_size = (224, 224),
+        color_mode = 'rgb',
+        batch_size = 8,
+        class_mode = 'categorical',
+    )
+
     return generator    
 
 def create_model():
     vgg16_model = keras.applications.vgg16.VGG16()
     model = Sequential()
 
-    for layer in vgg16.layer[:-1]:
+    for layer in vgg16_model.layers[:-3]:
         model.add(layer)
     
-    for layer in model.layer:
+    for layer in model.layers:
         layer.trainable = False
-
-    model.add(Dense(23, activation = 'sigmoid'))
     
-    model.compile(Adam(lr = .0001), loss = 'categorical_crossentropy', metrics = ['accuracy'])
+    model.add(Dense(4096, activation = 'relu'))
+    model.add(Dense(4096, activation = 'relu'))
+    model.add(Dense(23, activation = 'softmax'))
+    
+    model.compile(Adam(lr = .001), loss = 'binary_crossentropy', metrics = ['accuracy'])
 
     return model
 
 
 def load_compiled_model():
-    if MODEL_FILEPATH == 'int':
+    if MODEL_FILEPATH == 'none':
         model = create_model()
     else:
         model = load_model(MODEL_FILEPATH)
@@ -82,17 +104,14 @@ def train_model(model, train_generator, validation_generator):
 
 if __name__ == '__main__':
     # create train, validate and test generators
-    train_generator = create_generator(TRAIN_LOCATION)
-    validation_generator = create_generator(VALIDATION_LOCATION)
-    test_generator = create_generator(TEST_LOCATION)
-    # load pre-compiled model
+    train_generator = create_aug_generator(TRAIN_LOCATION)
+    validation_generator = create_aug_generator(VALIDATION_LOCATION)
+    test_generator = create_aug_generator(TEST_LOCATION)
+    # # load pre-compiled model
     model = load_compiled_model()
-    # train the model
+    # # train the model
     model_trained = train_model(model, train_generator, validation_generator)
-    # save the model
+    # # save the model
     save_trained_model(model_trained, 'models/clf_trained')
 
     print('Execution done!')
-
-
-
